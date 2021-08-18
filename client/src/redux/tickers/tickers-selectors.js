d3943d636fa8ea0570f98ae1c1324bb3397365b2
Vlad_Horii <vladhorii@yandex.ru>
2021-08-19 00:00:58 +0300

    Добавил селектор getPausedList

--- client/src/redux/tickers/tickers-selectors.js
+++ client/src/redux/tickers/tickers-selectors.js
@@ -1,6 +1,8 @@
 const getTickers = (state) => state.tickers.tickers;
+const getPausedList = (state) => state.tickers.pausedList;
 
 const exports = {
   getTickers,
+  getPausedList,
 };
 export default exports;
