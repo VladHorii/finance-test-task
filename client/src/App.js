d4f50c894b3b67a1dad3db58c07d3c905258e3e5
Vlad_Horii <vladhorii@yandex.ru>
2021-08-19 00:04:08 +0300

    Рефакторинг кода

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,65 @@
-// import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import { tickersActions, tickersSelectors } from "./redux/tickers";
 import "./App.css";
 
-import { Manager } from "socket.io-client";
 import TickersList from "./components/TickersList";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 
-const manager = new Manager("http://localhost:4000/", {
-  reconnectionDelayMax: 10000,
-});
-
-const socket = manager.socket("/", {});
+import { socket } from "./api/api";
+import NewTicker from "./components/NewTicker";
+import ChangeInterval from "./components/ChangeInterval";
 
 function App() {
   const tickers = useSelector(tickersSelectors.getTickers);
+  const pausedList = useSelector(tickersSelectors.getPausedList);
+  const [availableTickers, setAvailableTickers] = useState([]);
+  const isLoading = useRef(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     socket.emit("start");
 
     socket.on("ticker", (response) => {
+      if (!isLoading.current) {
+        isLoading.current = true;
+      }
       dispatch(tickersActions.refresh(response));
     });
   }, [dispatch]);
 
+  useEffect(() => {
+    const getTickersList = async () => {
+      const { data } = await axios.get(
+        "http://localhost:4000/allAvailableTickers"
+      );
+      setAvailableTickers(data.tickers);
+    };
+    getTickersList();
+  }, []);
+
+  useEffect(() => {
+    socket.emit("changePausedList", pausedList);
+  }, [pausedList]);
+
   return (
-    <>
-      <h1>Tickers list</h1>
-      {tickers.length > 0 && <TickersList tickers={tickers} />}
-    </>
+    <div className="App">
+      {availableTickers.length > 0 && (
+        <NewTicker newTickersList={availableTickers} />
+      )}
+
+      {!isLoading.current && tickers?.length === 0 && (
+        <h2>Загружаем список тикеров...</h2>
+      )}
+
+      {tickers?.length > 0 && (
+        <>
+          <h1>Tickers list</h1>
+          <TickersList tickers={tickers} />
+          <ChangeInterval />
+        </>
+      )}
+    </div>
   );
 }
 
