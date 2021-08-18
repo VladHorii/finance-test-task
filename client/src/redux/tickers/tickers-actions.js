3a47022dee1909d1256fe37442503e25b8c09f5d
Vlad_Horii <vladhorii@yandex.ru>
2021-08-19 00:01:35 +0300

    Добавление, удаление тикеров. PaudesList

--- client/src/redux/tickers/tickers-actions.js
+++ client/src/redux/tickers/tickers-actions.js
@@ -1,8 +1,39 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
+import { socket } from "../../api/api";
 
 const refresh = createAction("tickers/refresh");
+const addToPaysedList = createAction("tickers/pausedList/add");
+const removeFromPaysedList = createAction("tickers/pausedList/remove");
+
+const removeTicker = createAsyncThunk(
+  "tickers/remove",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      socket.emit("removeTicker", credentials);
+      return credentials;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+const addTicker = createAsyncThunk(
+  "tickers/add",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      socket.emit("addTicker", credentials);
+      return credentials;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
 const exportFunctions = {
   refresh,
+  addToPaysedList,
+  removeFromPaysedList,
+  removeTicker,
+  addTicker,
 };
 
 export default exportFunctions;
--- client/src/redux/tickers/tickers-reducer.js
+++ client/src/redux/tickers/tickers-reducer.js
@@ -4,8 +4,30 @@ import actions from "./tickers-actions";
 
 const tickers = createReducer([], {
   [actions.refresh]: (_, { payload }) => payload,
+  [actions.removeTicker.fulfilled]: (state, { payload }) =>
+    state.filter((el) => el.ticker !== payload),
+  [actions.addTicker.fulfilled]: (state, { payload }) => [
+    ...state,
+    {
+      ticker: payload,
+      exchange: "NASDAQ",
+      price: 0.0,
+      change: 0.0,
+      change_percent: 0.0,
+      dividend: 0.0,
+      yield: 0.0,
+      last_trade_time: "2021-01-01T00:00:01.000Z",
+    },
+  ],
+});
+
+const pausedList = createReducer([], {
+  [actions.addToPaysedList]: (state, { payload }) => [...state, payload],
+  [actions.removeFromPaysedList]: (state, { payload }) =>
+    state.filter((el) => el !== payload),
 });
 
 export default combineReducers({
   tickers,
+  pausedList,
 });
