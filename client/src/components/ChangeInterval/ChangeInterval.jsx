9d1349f2060b41f12bbbca8519a5a838de480acb
Vlad_Horii <vladhorii@yandex.ru>
2021-08-21 17:58:16 +0300

    Переписал компонент с использованием antd

--- client/src/components/ChangeInterval/ChangeInterval.jsx
+++ client/src/components/ChangeInterval/ChangeInterval.jsx
@@ -1,28 +1,33 @@
 import { useState } from "react";
+import { Form, InputNumber, Button } from "antd";
+import "antd/dist/antd.css";
 import { socket } from "../../api/api";
 
 const ChangeInterval = () => {
-  const [interval, setInterval] = useState("");
+  const [interval, setInterval] = useState(5000);
 
-  const handleSubmitInterval = (e) => {
-    e.preventDefault();
+  const handleSubmitInterval = () => {
     socket.emit("fetchInterval", Number(interval));
-    setInterval(() => "");
   };
 
   return (
-    <form onSubmit={handleSubmitInterval}>
-      <label>
-        Change update interval:
-        <input
-          type="number"
-          name="changeInterval"
-          value={interval}
-          onChange={(e) => setInterval(e.target.value)}
-        />
-      </label>
-      <button type="submit">Apply</button>
-    </form>
+    <>
+      <Form
+        name="customized_form_controls"
+        layout="inline"
+        onFinish={handleSubmitInterval}
+        initialValues={{ changeInterval: interval }}
+      >
+        <Form.Item label="Change update interval" name="changeInterval">
+          <InputNumber value={interval} onChange={setInterval} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Apply
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 export default ChangeInterval;
